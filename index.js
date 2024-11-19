e0f123cf0a396d98ab6bf5d6c28368426a0ade1e
JusGoWithTheFLo <florian.kanohotaong@gmail.com>
2024-11-19 16:45:32 -0500

    MEMORY CARD GAME
    Got the idea to create this game on the plane ride to Pittsburgh.  My son was getting bored of the tablet on the plane ride, so I wanted to see how quickly I could create the game.  All I had was my phone and bluetooth keyboard LOL.
    
    I finished it before the plane landed, but didn't have the json file or asset folder with the images to reference, so there were just a bunch of blocks on the screen. **sad face**
    
    As soon as I got home, I built it and downloaded the assets to play with my son. After a couple rounds, he asked if he could have 'ghost pikachu' on there, so I downloaded some pictures of ghost pokemon and added in the pokemonTCG backing to be the back of the card.
    
    After a few rounds of that (which he LOVED), he asked if a BIG ghost pikachu could popup.  I created a "You Win" alert when the score reaches 9, but I want to add something like a lightbox that pops up with the ghost pikachu and a YOU WIN banner.  I think he'd like that :D
    
    --created following tutorial; edited additional featuers on my own :D
    https://dev.to/javascriptacademy/creating-a-memory-card-game-with-html-css-and-javascript-57g1

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//-----------------------
+//-----DEFINING VARIABLES
+//-----------------------
 const gridContainer = document.querySelector(".grid-container");
 let cards = [];
 let firstCard, secondCard;
 let lockBoard = false;
 let score = 0;
-
 document.querySelector(".score").textContent = score;
 
+//-----------------------
+//-----FETCH CARD DATA
+//-----------------------
 fetch("./data/cards.json")
   .then((res) => res.json())
   .then((data) => {
@@ -14,6 +19,9 @@ fetch("./data/cards.json")
     generateCards();
   });
 
+//-----------------------
+//-----SHUFFLE CARDS
+//-----------------------
 function shuffleCards() {
   let currentIndex = cards.length,
     randomIndex,
@@ -27,6 +35,9 @@ function shuffleCards() {
   }
 }
 
+//-----------------------
+//-----GENERATE CARDS
+//-----------------------
 function generateCards() {
   for (let card of cards) {
     const cardElement = document.createElement("div");
@@ -43,6 +54,9 @@ function generateCards() {
   }
 }
 
+//-----------------------
+//-----FLIP CARD
+//-----------------------
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
@@ -55,26 +69,43 @@ function flipCard() {
   }
 
   secondCard = this;
-  score++;
-  document.querySelector(".score").textContent = score;
+  
   lockBoard = true;
 
   checkForMatch();
 }
 
+//-----------------------
+//-----CHECK FOR MATCH
+//-----------------------
 function checkForMatch() {
   let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
   isMatch ? disableCards() : unflipCards();
 }
 
+//-----------------------
+//-----DISABLE CARDS (IF MATCHED) & INCREASES SCORE
+//-----------------------
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
-
+  
+  //----------INCREASES SCORE :D
+  score++;
+  document.querySelector(".score").textContent = score;
+  
   resetBoard();
+
+  //----if score is 9, displays winning alert
+  if(score == 9){
+    alert("YOU WIN!");
+  }
 }
 
+//-----------------------
+//-----UNFLIP CARDS
+//-----------------------
 function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove("flipped");
@@ -83,12 +114,18 @@ function unflipCards() {
   }, 1000);
 }
 
+//-----------------------
+//-----RESET BOARD
+//-----------------------
 function resetBoard() {
   firstCard = null;
   secondCard = null;
   lockBoard = false;
 }
 
+//-----------------------
+//-----RESTART GAME
+//-----------------------
 function restart() {
   resetBoard();
   shuffleCards();
@@ -97,3 +134,10 @@ function restart() {
   gridContainer.innerHTML = "";
   generateCards();
 }
+
+
+//-----------------------
+//-----WIN SCREEN POPUP
+//-----------------------
+
+
